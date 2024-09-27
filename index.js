f69043198a3fb5a39c36ba9aa8e47113e0b0cffa
Andrew Davis <dynamicdebug@gmail.com>
2024-09-27 16:52:09 -0400

    starter files

---
+++ index.js
@@ -0,0 +1,8 @@
+const canvas = document.createElement('canvas');
+
+canvas.style.width = '500px;';
+canvas.style.height = '500px';
+
+canvas.style.border = '2px solid rebeccapurple';
+
+document.body.appendChild(canvas);
