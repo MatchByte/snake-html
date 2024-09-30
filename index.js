7ae12daf87e6dd83863f933864b5162b9351b9fa
Andrew Davis <dynamicdebug@gmail.com>
2024-09-30 12:12:15 -0400

    canvas context

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const canvas = document.createElement('canvas');
-
+const ctx = canvas.getContext('2d');
+ctx.fillStyle = 'green';
+ctx.fillRect(10, 10, 10, 10);
 canvas.style.width = '500px;';
 canvas.style.height = '500px';
 
 canvas.style.border = '2px solid rebeccapurple';
 
 document.body.appendChild(canvas);
-
-let querySelector;
